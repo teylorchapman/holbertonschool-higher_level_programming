6218e1224b588ed74a9eedd2eda38938fa4957c7
Teylor <teylorchapman@github.com>
2023-01-08 19:33:49 +0000

    maybe thisll print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 if alpha in range(ord('a'), ord('z') + 1):
-    lower = chr(alpha)
-    if lower not in "eq":
-        print(lower, end="")
+    while (i != 'e', i != 'q')
+print(chr(alpha).format(), end="")

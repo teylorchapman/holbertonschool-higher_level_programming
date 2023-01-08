ffe526b463b6415e0baa01153288400fb61a9385
Teylor <teylorchapman@github.com>
2023-01-08 19:35:35 +0000

    maybe thisll print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 if alpha in range(ord('a'), ord('z') + 1):
-    while (i != 'e', i != 'q')
-print(chr(alpha).format(), end="")
+    while (i != 'e', i != 'q'):
+        print(chr(alpha).format(), end="")

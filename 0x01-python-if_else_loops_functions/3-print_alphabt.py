f2c821a759fccd5e0678d23a366b49b3ec81e79f
Teylor <teylorchapman@github.com>
2023-01-08 19:47:47 +0000

    please god im losing hope

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for t in range(97, 123):
-    while (t != 'e' and t != 'q'):
-        print(chr(t).format(), end="")
+    if (t != 'e' and t != 'q'):
+        print(chr(t).format(), end='')

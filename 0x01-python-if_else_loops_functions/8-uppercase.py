4786a9fa95bf37a6f2272ad81d064d4f14275521
teylorchapman <teylorchapman@github.com>
2023-01-09 22:23:26 +0000

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for u in str:
         if ord(u) >= 97 and ord(u) <= 123:
-            u = chr(ord(u) - 33)
+            u = chr(ord(u) - 32)
         print("{}".format(u), end="")
     print("")

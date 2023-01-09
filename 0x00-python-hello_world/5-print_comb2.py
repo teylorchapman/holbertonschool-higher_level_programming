16c51899495b52996a85b8bf32bf1bf27c3e3cde
teylorchapman <teylorchapman@github.com>
2023-01-09 20:57:16 +0000

    fixed spacing

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for x in range(0, 99):
-    print("{}".format(x), end=", ")
-    print("99")
+for x in range(0, 100):
+    print("{:02d}".format(x), end=", ")
+    if x == 99:
+        print("{}".format(x))

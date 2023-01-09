f8a086792ccf2fde2fd45dd8dbcfc349959039cc
teylorchapman <teylorchapman@github.com>
2023-01-09 21:36:39 +0000

    this might work

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -2,6 +2,6 @@
 for x in range(0, 10):
     for y in range(x + 1, 10):
         if x == 8 and y == 9:
-            print("89")
-        elif x < y:
+            print("89".format(x, y))
+        else:
             print("{}{}".format(x, y, end=", "))

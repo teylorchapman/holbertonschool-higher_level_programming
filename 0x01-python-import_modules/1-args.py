cd18205952ff5fcd2e98f65b40b7e0cb8ee87d79
teylorchapman <teylorchapman@github.com>
2023-01-11 01:27:42 +0000

    args

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 if __name__ == "__main__":
     cb = len(sys.argv)
     if cb == 1:
         print("0 arguments.")
     elif cb == 2:
-        print("1 argument:\n1:{}".format(str(argv[1])))
+        print("1 argument:\n1:{}".format(str(sys.argv[1])))
     else:
         print("{} arguments:".format(cb - 1))
     for i in range(len(sys.argv)):

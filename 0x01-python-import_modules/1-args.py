e09565df37e1de6de0f33f84959e78681184aabe
teylorchapman <teylorchapman@github.com>
2023-01-11 01:25:16 +0000

    args

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    cb = len(sys.argv)
+    if cb == 1:
+        print("0 arguments.")
+    elif cb == 2:
+        print("1 argument:\n1:{}".format(str(argv[1])))
+    else:
+        print("{} arguments:".format(cb - 1))
+    for i in range(len(sys.argv)):
+        if i != 0:
+            print("{}: {}".format(i, str(sys.argv[i])))

d59f7cfb72c647a330b8424a3c4c9a3e6017de63
Teylor Chapman <111523192+teylorchapman@users.noreply.github.com>
2023-01-11 10:28:59 -0600

    Update 1-args.py

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if cb == 1:
         print("0 arguments.")
     elif cb == 2:
-        print("1 argument:\n1:{}".format(str(sys.argv[1])))
+        print("1 argument:\n1: {}".format(str(sys.argv[1])))
     else:
         print("{} arguments:".format(cb - 1))
         for i in range(len(sys.argv)):

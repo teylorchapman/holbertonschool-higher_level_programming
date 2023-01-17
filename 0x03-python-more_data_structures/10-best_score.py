a57edc65dc78fa679cd1b8ad1f831b65eea71b85
teylorchapman <teylorchapman@github.com>
2023-01-17 22:45:42 +0000

    10-best_score.py

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def best_schore(a_dictionary):
-    if a_dictionary:
-        return max(a_dictionary, key=lambda t: a_dictionary[t])
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=lambda t: a_dictionary[t])
+        

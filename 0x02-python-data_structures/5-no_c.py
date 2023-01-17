a1c59415137948564b8839e9165b65f444347ca1
teylorchapman <teylorchapman@github.com>
2023-01-17 05:01:13 +0000

    it helps if you actually press the right button

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for t in my_string:
-        if t != "c" and t !- "C":
+        if t != "c" and t != "C":
             new_string += t
         return new_string

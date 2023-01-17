39dae29c096f98a15a300ce5e5453b7d09f309d1
teylorchapman <teylorchapman@github.com>
2023-01-17 16:49:50 +0000

    please work this time

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
      new = [ ]
-     for e in range(len(my_list)):
-         return [e if e != search else replace for e in my_list]
+     for t in range(len(my_list)):
+         return [t if t != search else replace for t in my_list]

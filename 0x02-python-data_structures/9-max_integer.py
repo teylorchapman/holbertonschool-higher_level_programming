02a5c3bf0bae6d96ae3f3ed282e720dbefe46fe7
teylorchapman <teylorchapman@github.com>
2023-01-17 06:47:38 +0000

    biggest int

---
+++ 0x02-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        big_int = my_list[0]
+        for t in range(len(my_list)):
+            if my_list[t] > big_int:
+                big_int = my_list[t]
+        return big_int

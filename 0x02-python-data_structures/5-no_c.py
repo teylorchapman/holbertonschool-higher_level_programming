dac56343de7741a5d3a0f407dbed5b6bf05427f1
teylorchapman <teylorchapman@github.com>
2023-01-17 06:10:32 +0000

    maybe be more specific?

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for t in my_string:
-        if t != "c" and t != "C":
-            new_string += t
-        return new_string
+    if my_string:
+        for t in my_string:
+            if t != 'C' and t != 'c':
+                new_string += t
+    return new_string

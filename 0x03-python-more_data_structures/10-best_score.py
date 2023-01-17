64b9488f1905695088291ea0d9aa941fc82d0dd8
teylorchapman <teylorchapman@github.com>
2023-01-17 22:47:06 +0000

    this time it should work

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def best_schore(a_dictionary):
-    if not a_dictionary:
-        return None
-    return max(a_dictionary, key=lambda t: a_dictionary[t])
+def best_score(a_dictionary):
+    if  a_dictionary:
+        return max(a_dictionary, key=lambda t: a_dictionary[t])
         

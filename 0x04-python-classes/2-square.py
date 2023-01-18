2471e27ac86540fdc0efac014e6edce1c76df844
teylorchapman <teylorchapman@github.com>
2023-01-18 01:58:10 +0000

    fixed spacing and pep8 compliance

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -12,7 +12,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-
-        def area(self):
-            """this gives the area of a square"""
-            return (self.__size**2)
+    def area(self):
+        """this gives the area of a square"""
+        return (self.__size**2)

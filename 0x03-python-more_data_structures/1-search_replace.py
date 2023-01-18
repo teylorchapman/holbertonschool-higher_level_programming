30637e82cbd2f033ada77c3bd415854e1860f9fd
teylorchapman <teylorchapman@github.com>
2023-01-18 23:59:39 +0000

    fixing pep8 compliance

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-     return [t if t != search else replace for t in my_list]
+    return [t if t != search else replace for t in my_list]

1bcc2444f830d5d83105364de32d7162dfbcfb23
teylorchapman <teylorchapman@github.com>
2023-01-18 01:09:51 +0000

    fixed output

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -6,7 +6,7 @@ class Square:
     """this class defines a square"""
     def __init__(self, size=0):
         """this initilizes a simple square"""
-        if size != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")

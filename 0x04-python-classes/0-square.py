199b766c5e58813d05d3df64f0f1ddd637970fc9
teylorchapman <teylorchapman@github.com>
2023-01-18 00:46:07 +0000

    simple square

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this is a square class that defines a square :)"""
+
+class Square:
+    """this class defines a square"""
+    def __init__(self, size=0):
+        """this initializes a simple square"""
+        self.__size = size

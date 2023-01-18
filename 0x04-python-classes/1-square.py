351c0bb773372d4abc0ba71b9ae19c0575613c5e
teylorchapman <teylorchapman@github.com>
2023-01-18 01:03:29 +0000

    squared

---
+++ 0x04-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this is a square class that defines a square :)"""
+
+
+class Square:
+    """this class defines a square"""
+    def __init__(self, size=0):
+        """this initilizes a simple square"""
+        if size != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

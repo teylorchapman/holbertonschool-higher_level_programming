83cd8d3477c450ce43b8ddd7e0cca1490b6f736f
teylorchapman <teylorchapman@github.com>
2023-01-18 00:48:16 +0000

    fixing pep8 validation

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """this is a square class that defines a square :)"""
 
+
 class Square:
     """this class defines a square"""
     def __init__(self, size=0):
---
+++ 0x04-python-classes/tests/oop_method.py
@@ -0,0 +1,5 @@
+class Person:
+    def say_hi(self):
+        print('Hello, how are you?')
+
+p = Person().say_hi()

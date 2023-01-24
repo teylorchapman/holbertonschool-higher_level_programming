8e6fb82e8e47921b1adbd7d80ecb537b2b39308e
teylorchapman <teylorchapman@github.com>
2023-01-24 19:58:49 +0000

    one more time

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
 
                 @property
                 def width(self):
-                        return(self.__width)
+                        return self.__width
 
                 @width.setter
                 def width(self, value):

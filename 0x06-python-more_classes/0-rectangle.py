5005a1cebb248224aed3a16b7163fb12954331a7
teylorchapman <teylorchapman@github.com>
2023-01-24 18:19:43 +0000

    okay now i think i fixed it

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -8,18 +8,18 @@ class Rectangle:
                 """initializes the height and width of the rectangle"""
                 if type(width) != int:
                         raise TypeError("width must be an integer")
-                if type(width) < 0:
+                if width < 0:
                         raise ValueError("width must be >= 0")
                 if type(height) != int:
                         raise TypeError("height must be an integer")
-                if type(height) < 0:
+                if height < 0:
                         raise ValueError("height must be >= 0")
 
                 self.width = width
                 self.height = height
 
                 @property
-                def size(value):
+                def width(value):
                         return(self.__width)
 
                 @width.setter
@@ -38,6 +38,6 @@ class Rectangle:
                 def height(self, value):
                         if type(value) != int:
                                 raise TypeError("height must be an integer")
-                        if type(value) < 0:
+                        if value < 0:
                                 raise ValueError("height must be >= 0")
                         self.__height = value

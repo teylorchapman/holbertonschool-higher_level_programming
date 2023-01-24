451b7045dbc92a648e65dc00644c8f575f2f4215
teylorchapman <teylorchapman@github.com>
2023-01-24 20:04:52 +0000

    im losing motivation

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -6,11 +6,11 @@ class Rectangle:
         """Rectangle Class, holds height and width"""
         def __init__(self, width=0, height=0):
                 """initializes the height and width of the rectangle"""
-                if type(width) != int:
+                if type(width) is not int:
                         raise TypeError("width must be an integer")
                 if width < 0:
                         raise ValueError("width must be >= 0")
-                if type(height) != int:
+                if type(height) is not int:
                         raise TypeError("height must be an integer")
                 if height < 0:
                         raise ValueError("height must be >= 0")
@@ -23,7 +23,7 @@ class Rectangle:
 
                 @width.setter
                 def width(self, value):
-                        if type(value) != int:
+                        if type(value) is not int:
                                 raise TypeError("width must be an integer")
                         if value < 0:
                                 raise ValueError("width must be >= 0")
@@ -35,7 +35,7 @@ class Rectangle:
 
                 @height.setter
                 def height(self, value):
-                        if type(value) != int:
+                        if type(value) is not int:
                                 raise TypeError("height must be an integer")
                         if value < 0:
                                 raise ValueError("height must be >= 0")

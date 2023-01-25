4f5ef44789d3f85a1818c2b6abaf6afd1c048afc
teylorchapman <teylorchapman@github.com>
2023-01-25 23:59:06 +0000

    say my name, say my name when nobody is around you

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""say_my_name module"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function that prints first and last name"""
+    if type(first_name) not in [str]:
+        raise TypeError("first_name must be a string")
+    if type(last_name) not in [str]:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
+    

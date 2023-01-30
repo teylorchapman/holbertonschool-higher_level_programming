ae382cb54bccef214b090a0bfb71d9b1e8788f8d
teylorchapman <teylorchapman@github.com>
2023-01-30 21:01:03 +0000

    documentation added

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""A function that returns the list of available attributes and methods
-of an object"""
+"""This is a function that returns the list of available attributes and methods
+of an object."""
 
 
 def lookup(obj):
+    """This method will return all properties and methods of the specified
+    object, without the values"""
     return dir(obj)

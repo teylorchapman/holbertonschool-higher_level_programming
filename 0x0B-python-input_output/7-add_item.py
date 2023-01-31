b4c179a5e317a4718a6aa0ee4517cf41f6cc3d57
teylorchapman <teylorchapman@github.com>
2023-01-31 23:55:46 +0000

    add item one more time

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,14 +3,17 @@
 then saves them to a file"""
 
 
-from sys import argv
+import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 try:
-    j = load_from_json_file("add_item.json")
-except:
-    j = []
+    i = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    i = []
 
-save_to_json_file(j + argv[1:], "add_item.json")
+for a in sys.argv[1:]:
+    i.append(a)
+
+save_to_json_file(i, "add_item.json")

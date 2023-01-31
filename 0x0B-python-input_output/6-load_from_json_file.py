b996bc498ba07bec1aa576a68dafd605adf13c17
teylorchapman <teylorchapman@github.com>
2023-01-31 23:28:20 +0000

    load json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""creates an object from a JSON file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an object from a json file"""
+    with open(filename, "r", encoding="") as f:
+        return json.load(f)

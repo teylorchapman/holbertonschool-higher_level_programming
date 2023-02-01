83613655cf8447cb6a2c1716a424ca3f443be7db
teylorchapman <teylorchapman@github.com>
2023-02-01 00:28:52 +0000

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/urs/bin/python3
+"""Student class that defines a student"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return (vars(self))
+        newdict = {}
+        for x in attrs:
+            try:
+                newdict[x] = vars(self)[x]
+            except Exception:
+                pass
+            return newdict

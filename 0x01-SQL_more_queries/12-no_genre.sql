50d3c7fbc829ac3fd0c761ab8e0ba69b6d2bd340
teylorchapman <5535@holbertonstudent.com>
2023-02-14 17:44:38 -0800

    trying it to the left?

--- 0x01-SQL_more_queries/12-no_genre.sql
+++ 0x01-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- lists all shows that dont have a linked genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;

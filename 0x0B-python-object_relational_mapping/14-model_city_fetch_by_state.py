03902c4d48150df66d64221cf136a86e80609fa6
teylorchapman <5535@holbertonstudent.com>
2023-02-28 17:23:43 -0800

    git outta town

--- 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,13 +7,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-
     Session = sessionmaker(bind=engine)
     session = Session()
     for city, state in session.query(City, State) \
             .filter(State.id == City.state_id).order_by(City.id):
-        print("{}: ({} {}".format(state.name, city.id, city.name))
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
--- 0x0B-python-object_relational_mapping/model_city.py
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
 """contains class definition of a city"""
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import String, Integer, Column, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import ForeignKey
-
 Base = declarative_base()
 
 
 class City(Base):
-    """City class"""
+    """subclass of Base"""
     __tablename__ = 'cities'
-    id = Column(Integer,
-                autoincrement=True,
-                unique=True,
-                nullable=False,
-                primary_key=True
-                )
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer,
-                      ForeignKey('states.id'),
-                      nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)

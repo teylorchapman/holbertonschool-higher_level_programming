09a204b47e9bee8728a57faacf6caa27d3cf5ef9
teylorchapman <5535@holbertonstudents.com>
2023-03-11 05:31:21 +0000

    oopsie daisy

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -2,12 +2,13 @@
 """displays the value of the X-Request-Id"""
 
 
-import urllib.request
-from sys import argv
+if __name__ == "__main__":
+    import urllib.request
+    from sys import argv
 
-uriel = argv[1]
+    uriel = argv[1]
 
-with urllib.request.urlopen(uriel) as resp:
-    head = resp.info()
-    req_id = head['X-Request-Id']
-    print(req_id)
+    with urllib.request.urlopen(uriel) as resp:
+        head = resp.info()
+        req_id = head['X-Request-Id']
+        print(req_id)

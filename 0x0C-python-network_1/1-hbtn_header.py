2f98116e4404d147e38e492919fc87870bb24119
teylorchapman <5535@holbertonstudents.com>
2023-03-11 05:29:47 +0000

    this is alright

---
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""displays the value of the X-Request-Id"""
+
+
+import urllib.request
+from sys import argv
+
+uriel = argv[1]
+
+with urllib.request.urlopen(uriel) as resp:
+    head = resp.info()
+    req_id = head['X-Request-Id']
+    print(req_id)

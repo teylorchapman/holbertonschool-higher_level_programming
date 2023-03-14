b9061332e8b14c12dee07e9d6676cfad8d2ad3dd
teylorchapman <5535@holbertonstudents.com>
2023-03-14 01:41:44 +0000

    wtfigo

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -3,8 +3,7 @@
 import requests
 
 if __name__ == "__main__":
-    resp = requests.get("https://intrannet.hbtn.io/status")
-
-    print("Body response:")
-    print("\t- type: {}".format(type(resp.text)))
-    print("\t- content: {}".format(resp.text))
+    resp = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(resp.text)))
+    print('\t- content: {}'.format(resp.text))

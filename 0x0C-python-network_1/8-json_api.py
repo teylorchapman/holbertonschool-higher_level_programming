a4b84f7a92721b96d09c973454ef664fe99a5482
teylorchapman <5535@holbertonstudents.com>
2023-03-14 02:23:40 +0000

    json api

---
+++ 0x0C-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a ```POST```
+request to ```https://0.0.0.0:5000/search_user```
+with the letter as the parameter"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://0.0.0.0:5000/search_user"
+    if len(argv) == 1:
+        q = ""
+    else:
+        q = argv[1]
+
+    resp = requests.post(url, data={'q': q})
+    try:
+        dict = resp.json()
+        id = dict.get('id')
+        name = dict.get('name')
+        if len(dict) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(id, name))
+    except Exception as c:
+        print("Not a valid JSON")

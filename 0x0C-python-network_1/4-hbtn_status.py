7e56a6b4f53b6dc8a4d6d23b65291080035f6e19
teylorchapman <5535@holbertonstudents.com>
2023-03-14 01:38:51 +0000

    i aint doin it right lmao

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -4,8 +4,7 @@ import requests
 
 if __name__ == "__main__":
     resp = requests.get("https://intrannet.hbtn.io/status")
-    json_cont = resp.jsons()
 
     print("Body response:")
-    print("\t- type: {}".format(type(json_cont)))
-    print("\t- content: {}".format(json_cont))
+    print("\t- type: {}".format(type(resp.text)))
+    print("\t- content: {}".format(resp.text))

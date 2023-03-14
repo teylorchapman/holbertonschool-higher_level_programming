d37d7ffe028bca9dc386d6632e14cc99b011c132
teylorchapman <5535@holbertonstudents.com>
2023-03-14 02:28:31 +0000

    idk

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -7,20 +7,21 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://0.0.0.0:5000/search_user"
+    url = "http://0.0.0.0:5000/search_user"
     if len(sys.argv) == 1:
         q = ""
     else:
         q = sys.argv[1]
 
-    resp = requests.post(url, data={'q': q})
+    req = requests.post(url, data={'q': q})
     try:
-        dict = resp.json()
+        dict = req.json()
         id = dict.get('id')
         name = dict.get('name')
         if len(dict) == 0:
             print("No result")
         else:
             print("[{}] {}".format(id, name))
-    except Exception as c:
+    except Exception as e:
         print("Not a valid JSON")
+

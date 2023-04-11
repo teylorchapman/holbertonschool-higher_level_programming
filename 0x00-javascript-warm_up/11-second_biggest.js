13bb7faac214738618d74f4323b8af4dc197094e
teylorchapman <5535@holbertonstudent.com>
2023-04-10 22:52:41 -0700

    asdfakjs two

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -4,22 +4,22 @@ const args = process.argv.slice(2);
 if (args.length < 2) {
   console.log(0);
 } else {
-  let one = parseInt(args[0]);
-  let two = parseInt(args[1]);
+  let first = parseInt(args[0]);
+  let second = parseInt(args[1]);
 
-  if (two > one) {
-    [one, two] = [two, one];
+  if (second > first) {
+    [first, second] = [second, first];
   }
 
   for (let i = 2; i < args.length; i++) {
     const numba = parseInt(args[i]);
-    if (numba > one) {
-      two = one;
-      one = numba;
-    } else if (numba > two) {
-      two = numba;
+    if (numba > first) {
+      second = first;
+      first = numba;
+    } else if (numba > second) {
+      second = numba;
     }
   }
 
-  console.log(two);
+  console.log(second);
 }

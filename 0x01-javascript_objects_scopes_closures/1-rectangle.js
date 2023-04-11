2ba081a503327e88f61d3b3378755c896e33b8a8
teylorchapman <5535@holbertonstudent.com>
2023-04-11 08:05:03 -0700

    semistandard fix :/

--- 0x01-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (wid, hei) {
-        this.width = wid;
-        this.height = hei;
-    }
+  constructor (wid, hei) {
+    this.width = wid;
+    this.height = hei;
+  }
 }
 
 module.exports = Rectangle;

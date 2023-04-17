feb9b25a1e420d5760eeb8764c3ddbc74cc6428e
teylorchapmang <5535@holbertonstudents.com>
2023-04-17 04:43:03 +0000

    fixing the semistandard

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const filepath = process.argv[2];
 const filecontent = process.argv[3];
 
 fs.writeFile(filepath, filecontent, 'utf-8', (err) => {
-    if (err) {
-        console.error(err);
-    }
-})
+  if (err) {
+    console.error(err);
+  }
+});
--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -5,20 +5,20 @@ const APIurl = process.argv[2];
 const characterid = 18;
 
 request(`${APIurl}?format=json`, (err, resp, body) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  
-    if (resp.statusCode !== 200) {
-      console.error(`Unexpected status code: ${resp.statusCode}`);
-      return;
-    }
-  
-    const movies = JSON.parse(body).results;
-    const wedgiemovies = movies.filter(movie =>
-      movie.characters.some(characterurl => characterurl.includes(`/${characterid}/`))
-    );
-  
-    console.log(`${wedgiemovies.length}`);
-  });
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  if (resp.statusCode !== 200) {
+    console.error(`Unexpected status code: ${resp.statusCode}`);
+    return;
+  }
+
+  const movies = JSON.parse(body).results;
+  const wedgiemovies = movies.filter(movie =>
+    movie.characters.some(characterurl => characterurl.includes(`/${characterid}/`))
+  );
+
+  console.log(`${wedgiemovies.length}`);
+});
